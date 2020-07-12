b3261cd76f40ec78481bacf31ce2cccd3776d23c
Denis Evsyukov <denis@evsyukov.org>
2020-07-12 16:51:22 +0300

    [~] deploy to s3deploy

---
+++ ci-install-s3deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+S3DEPLOY_DOWNLOAD=s3deploy_${S3DEPLOY_VERSION}_Linux-64bit.tar.gz
+
+set -x
+set -e
+
+wget https://github.com/bep/s3deploy/releases/download/v${S3DEPLOY_VERSION}/${S3DEPLOY_DOWNLOAD}
+tar xvzf ${S3DEPLOY_DOWNLOAD} s3deploy
